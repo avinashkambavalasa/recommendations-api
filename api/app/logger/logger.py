1941af6d6a4b723f5cdbed69039cce4b14767fd3
Avinash Kambavalasa <avinash_kambavalasa@mcafee.com>
2026-06-27 23:27:50 -0400

    add error logs stack trace

--- api/app/logger/logger.py
+++ api/app/logger/logger.py
@@ -16,3 +16,14 @@ def log_event(event_type: str, **fields: Any) -> None:
             sort_keys=True,
         )
     )
+
+
+def log_exception(event_type: str, **fields: Any) -> None:
+    logger.exception(
+        json.dumps(
+            {"event_type": event_type, **fields},
+            default=str,
+            separators=(",", ":"),
+            sort_keys=True,
+        )
+    )
